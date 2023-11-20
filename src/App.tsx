075aa1000be7e23d51b26c9733e7a89921e848a3
FinnKrause <finn_krause@gmx.de>
2023-11-21 00:40:57 +0100

    Fist working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+interface AppProps {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
 }
 
-export default App
+const App:React.FC<AppProps> = ():JSX.Element => {
+  const dateToCountdown = new Date("Dec 23, 2023 21:55:00").getTime();
+  const [timeLeft, setTimeLeft] = useState<number>(0);
+  const [randomURL, setRandomURL] = useState<string>("");
+
+  const getRandomImageURL = () => {
+    const url = "/BackgroundImages/Image (" + Math.floor(Math.random() * 16 + 1) + ")." + (Math.floor((Math.random()*1000)) < 0.5 ? "JPEG" : "JPG");
+    console.log(url);
+    setRandomURL(url);
+  }
+
+  const formatNumber = (num: number):string => {
+    if (num <= 9) return "0"+num;
+    else return num.toString();
+  }
+
+  useEffect(() => {
+    console.log("UseEffect run!")
+    setInterval(() => {
+      setTimeLeft(dateToCountdown - new Date().getTime());
+    }, 1000);
+
+    getRandomImageURL();
+    setInterval(() => {
+      getRandomImageURL();
+    }, 1000*60*60);
+    
+  }, [])
+  
+  return <div className="Wrapper">
+    {randomURL && <div className="Background" style={{backgroundImage: `url("${randomURL}")`}}/>}
+    <div className="Content" onClick={() => getRandomImageURL()}>
+
+      <div className="Digit">
+        <h2 className="Title">JOURS</h2>
+        <h1 className="Timer" id="Days">{formatNumber(Math.floor(timeLeft / (1000 * 60 * 60 * 24)))}</h1>
+      </div>
+      <h1 className="Timer Colon">:</h1>
+
+      <div className="Digit">
+        <h2 className="Title">HEURES</h2>
+        <h1 className="Timer" id="Hours">{formatNumber(Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)))}</h1>
+      </div>
+      <h1 className="Timer Colon">:</h1>
+
+      <div className="Digit">
+        <h2 className="Title">MINUTES</h2>
+        <h1 className="Timer" id="Minutes">{formatNumber(Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)))}</h1>
+      </div>
+        <h1 className="Timer Colon">:</h1>
+
+      <div className="Digit">
+        <h2 className="Title">SECONDES</h2>
+        <h1 className="Timer" id="Seconds">{formatNumber(Math.floor((timeLeft % (1000 * 60)) / 1000))}</h1>
+      </div>
+
+    </div>
+  </div>
+}
+
+export default App;
